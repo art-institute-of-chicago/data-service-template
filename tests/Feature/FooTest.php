605b9d745243aec7dfceb878852f90db38677f6b
Illya Moskvin <imoskvin@artic.edu>
2022-01-25 15:04:34 -0600

    Add stub detail endpoint test [API-245]

--- tests/Feature/FooTest.php
+++ tests/Feature/FooTest.php
@@ -2,14 +2,26 @@
 
 namespace Tests\Feature;
 
+use App\Models\Foo;
 use Tests\TestCase;
 
 class FooTest extends TestCase
 {
     public function test_example()
     {
-        $response = $this->get('/');
+        $foo = Foo::factory()->create([
+            'id' => 42,
+            'title' => 'Example Title',
+        ]);
 
-        $response->assertStatus(404);
+        $response = $this->getJson('api/v1/foos/' . $foo->id);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => [
+                'id' => 42,
+                'title' => 'Example Title',
+            ],
+        ]);
     }
 }
